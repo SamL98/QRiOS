fe853d0920908d5049441acdf0cd0489a66e6329
Sam Lerner <lerner98@gmail.com>
2017-10-21 14:11:58 -0400

    first commit

--- CustomQR/ViewController.swift
+++ CustomQR/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    var session: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureDevice = AVCaptureDevice.default(for: .video)
+        var input: AVCaptureDeviceInput
+        
+        do {
+            input = try AVCaptureDeviceInput(device: captureDevice!)
+        } catch let error as NSError {
+            print(error)
+            return
+        }
+        
+        session = AVCaptureSession()
+        session.addInput(input)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.frame = view.layer.bounds
+        previewView.layer.addSublayer(previewLayer)
+        
+        session.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
